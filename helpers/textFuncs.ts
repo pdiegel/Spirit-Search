6ce1b4e2d8d1b5c9163cc0797b8d96920ff25646
Philip Diegel <philipdiegel@gmail.com>
2024-03-21 19:24:59 -0400

    Full textFuncs Test Coverage

--- helpers/textFuncs.ts
+++ helpers/textFuncs.ts
@@ -8,13 +8,13 @@ export const breakTextIntoTwoSentenceChunks = function (
   // The ingredient descriptions are returned as a single large block of text.
   // This function will break the text into chunks of two sentences each, so that
   // the description can be displayed in a more readable format.
-  const sentences = text.match(/[^.!?]+[.!?]+\s*(?=[A-Z])/g) || [text];
+  const sentences = text.match(/[^.!?]+[.!?]+(\s|$)/g) || [text];
 
-  // Group the sentences into chunks of two
-  const chunks = [];
+  // Group the sentences into chunks of two. If there are less than 3 sentences,
+  // the text will not be broken up.
+  const chunks: string[] = [];
   for (let i = 0; i < sentences.length; i += 2) {
-    // Concatenate two sentences at a time, or just one if it's the last sentence without a pair.
-    chunks.push(sentences.slice(i, i + 2).join(" "));
+    chunks.push(sentences.slice(i, i + 2).join(""));
   }
 
   return chunks.map((s) => s.trim());
